8bc18d1778aa509cd8f3c7bc46845917285e2963
Krishnapal Singh Sisodiya <126499943+Krishna9174@users.noreply.github.com>
2023-08-24 23:46:41 +0530

    Time: 12 ms (63.67%), Space: 8.4 MB (23.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans =0;
-        int prev=0;
-        unordered_map<char , int> mpp;
-        mpp['M'] = 1000;
-        mpp['D'] = 500;
-        mpp['C'] = 100;
-        mpp['L'] = 50;
-        mpp['X'] = 10;
-        mpp['V'] = 5;
-        mpp['I'] = 1;
-
-        for(int i=s.length()-1;i>=0; i--){
+        int n=s.length();
+        int ans=0;
+        int pre=0;
+        map<char, int> mpp;
+        mpp['I']=1;
+        mpp['V']=5;
+        mpp['X']=10;
+        mpp['L']=50;
+        mpp['C']=100;
+        mpp['D']=500;
+        mpp['M']=1000;
+        for(int i=n-1; i>=0; i--){
             char current=s[i];
-            int value=mpp[current];
-            if(value>=prev){
-                ans+=value;
-            }else{
-                ans-=value;
-            }
-            prev=value;
+             int value=mpp[current];
+             if(value>=pre){
+                 ans+=value;
+             }else{
+                 ans-=value;
+             }
+             pre=value;
         }
-        return ans;
+   return ans;
     }
 };
