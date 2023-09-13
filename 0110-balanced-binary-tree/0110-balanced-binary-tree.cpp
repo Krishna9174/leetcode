47179543ff840384f38ba74160d6b48d2dc883f7
Krishnapal Singh Sisodiya <126499943+Krishna9174@users.noreply.github.com>
2023-09-13 12:15:27 +0530

    Time: 12 ms (48.2%), Space: 22.2 MB (5.4%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,23 +12,51 @@
 class Solution {
 
 public:
-int height(TreeNode* root){
-    if(root==NULL) return 0;
-    int opt1=height(root->left);
-    int opt2=height(root->right);
-    int ans=max(opt1, opt2)+1;
-    return ans;
-}
+pair<bool, int> Balanced(TreeNode* root){
+    if(root==NULL){
+        pair<bool, int> p=make_pair(true, 0);
+        return p;
+    }
+      pair<bool, int> left=Balanced( root->left);
+      pair<bool, int> right=Balanced( root->right);
+      bool leftans=left.first;
+       bool rightans=right.first;
+      bool diff=abs(left.second - right.second)<=1;
 
+        pair<bool, int> ans;
+        ans.second=max(left.second, right.second)+1;
+        if(leftans && rightans && diff) ans.first=true;
+        else ans.first=false;
+        return ans;
+    
+}
     bool isBalanced(TreeNode* root) {
-        if(root==NULL){
-            return true;
-        }
-        int opt1=isBalanced(root->left);
-        int opt2=isBalanced(root->right);
-        int opt3=abs(height(root->left)- height(root->right))<=1;
-        if(opt1 && opt2 && opt3)  return true;
-        return false;
+        return Balanced(root).first;
 
     }
 };
+
+
+// class Solution {
+
+// public:
+// int height(TreeNode* root){
+//     if(root==NULL) return 0;
+//     int opt1=height(root->left);
+//     int opt2=height(root->right);
+//     int ans=max(opt1, opt2)+1;
+//     return ans;
+// }
+
+//     bool isBalanced(TreeNode* root) {
+//         if(root==NULL){
+//             return true;
+//         }
+//         int opt1=isBalanced(root->left);
+//         int opt2=isBalanced(root->right);
+//         int opt3=abs(height(root->left)- height(root->right))<=1;
+//         if(opt1 && opt2 && opt3)  return true;
+//         return false;
+
+//     }
+// };
