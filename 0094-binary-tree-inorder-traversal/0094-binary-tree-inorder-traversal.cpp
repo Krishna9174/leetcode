12e92446a3c654015a600b86c09b9b74ba2fec32
Krishnapal Singh Sisodiya <126499943+Krishna9174@users.noreply.github.com>
2023-09-18 19:03:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (12.39%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,17 +11,42 @@
  */
 class Solution {
 public:
-   void inorder(TreeNode* &root, vector<int> &ans){
-       if(root==NULL) return;
-       inorder(root->left,ans);
-       ans.push_back(root->val);
-       inorder(root->right,ans);
+//    void inorder(TreeNode* &root, vector<int> &ans){
+//        if(root==NULL) return;
+//        inorder(root->left,ans);
+//        ans.push_back(root->val);
+//        inorder(root->right,ans);
 
-   }
+//    }
 
     vector<int> inorderTraversal(TreeNode* root) {
          vector<int> ans;
-         inorder(root, ans);
+        //  inorder(root, ans);
+        //  return ans;
+        TreeNode* curr, *pre;
+         if(root==NULL) return ans;
+         curr=root;
+         while(curr!=NULL){
+             if(curr->left==NULL){
+             ans.push_back(curr->val);
+             curr=curr->right;
+             }
+             else{
+                 pre=curr->left;
+                 while(pre->right!=NULL && pre->right!=curr){
+                   pre=pre->right;}
+                   if(pre->right==NULL){
+                       pre->right=curr;
+                       curr=curr->left;
+                   }
+                   else{ pre->right=NULL;
+                       ans.push_back(curr->val);
+                       curr=curr->right;
+                   }
+                 }
+             }
+
+         
          return ans;
     }
 };
